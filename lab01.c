78019b802f6f240f25f8803f2e19015f60d616e3
octaviopalmieri00-ops <octaviopalmieri00@gmail.com>
2026-08-13 23:08:18 +0000

    inclusão do nome do curso

--- lab01.c
+++ lab01.c
@@ -6,8 +6,8 @@
 int main() {
   // imprime algumas mensagens na tela
   printf("Mackenzie - FCI\n");
-  printf("Algoritmos e ");
-  printf("Prog. II\n");
+  printf("Ciência da Computação\n");
+  printf("Algoritmos e programação II\n");
 
 
 
